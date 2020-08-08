2ce7affeb674e1c7975d4aaede684b44a6c5e368
Rafael Asencio <Rafael Asencio>
2020-08-08 21:18:51 +0200

    CoreData practices

---
+++ CoreDataTutorial/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Rafael on 01/08/2020.
+//  Copyright © 2020 RafaelAB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
